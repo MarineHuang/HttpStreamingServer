18fb67c7f8bf1a55b9b6baf9b19d76c28c94a085
MarineHuang <huang_yx005@163.com>
2022-10-11 19:54:49 +0800

    prod2 configuration is not needed

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
-BUILD=prod2
-sudo rm -rf Videos
-git checkout Videos
-ls ./Videos | grep -v Dune.mp4 | xargs -i rm ./Videos/{} -r
+BUILD=prod
 if [ "$BUILD" = "prod" ]; then
     docker-compose -f docker-compose-$BUILD.yml build
 fi
-docker-compose -f docker-compose-$BUILD.yml down -v 
+docker-compose -f docker-compose-$BUILD.yml down -v  
 docker-compose -f docker-compose-$BUILD.yml run --rm web ./wait-for-it.sh db:5432 -- python3 manage.py migrate
 docker-compose -f docker-compose-$BUILD.yml run --rm web python3 manage.py createsuperuser
-docker-compose -f docker-compose-$BUILD.yml up 
+docker-compose -f docker-compose-$BUILD.yml up -d
