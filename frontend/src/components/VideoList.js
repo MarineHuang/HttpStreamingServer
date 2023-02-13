26afdc0ea8acaf201688e60ca12fc11039f65b21
MarineHuang <huang_yx005@163.com>
2023-02-13 15:34:46 +0800

    support loadMore feature by react-infinite-scroller

--- frontend/src/components/VideoList.js
+++ frontend/src/components/VideoList.js
@@ -2,90 +2,77 @@ import React, { Component } from 'react';
 //import Slider from "react-slick";
 //import SampleNextArrow from "./SampleNextArrow";
 //import SamplePrevArrow from "./SamplePrevArrow";
+import InfiniteScroll from 'react-infinite-scroller'
 import './VideoList.css';
 
 
 export default  class VideoList extends Component {
 
     //this variable must be the same as PAGE_SIZE in settings.py
-    SLIDES_OF_CAROUSEL = 5;
+    //SLIDES_OF_CAROUSEL = 5;
 
     constructor(props) {
         super(props);
         this.state = {
+            hasMore: true,
             pager: this.props.pager,
             videos: this.props.videos
         };
-        this.afterChangeMethod = this.afterChangeMethod.bind(this);
+        //this.loadMoreData = this.loadMoreData.bind(this);
     };
 
 
-
-    componentWillReceiveProps(nextProps) {
-        const chooseIndex = (reset) =>{
-            if(reset === true){
-               return this.state.index;
-            }
-            return 0;
-        };
-        if (nextProps.videos !== this.props.videos) {
-            const index = chooseIndex(nextProps.reset);
+    // 加载更多数据
+    async loadMoreData(){
+        let pager = this.state.pager;
+        if( !pager.nextPageUrl ) {
+            this.setState({hasMore: false})
+            console.log("has no more data")
+            return
+        }
+        // API call to retrieve more videos
+        try {
+            console.log("loading more data: " + pager.nextPageUrl);
+            await pager.getNextPage();
+            let videos = this.state.videos;
+            videos.push(...pager.videos);
             this.setState({
-                pager: nextProps.pager,
-                videos: nextProps.videos
+                pager: pager,
+                videos: videos
             });
+        } catch(error) {
+            this.setState({hasMore: false})
+            console.log(error);
         }
-    };
+    }
 
-
-    /**
-     * this method is called by react slick after the slider finish transition
-     * used to compute if we need to make new API calls
-     * @param index
-     * @returns {Promise<void>}
-     */
-    async afterChangeMethod(index) {
-        const setSeriePagerIndex = (index) =>{
-            if(this.state.pager.type === 'Serie'){
-                this.setState({
-                    index: index
-                });
-            }
-        };
-        const isLastPage = (index + this.SLIDES_OF_CAROUSEL) === this.state.videos.length;
-        setSeriePagerIndex(index);
-        if (isLastPage && this.state.pager.nextPageUrl){
-            // API call to retrieve more videos when navigating through carousel
-            try {
-                let pager = this.state.pager;
-                await pager.getNextPage();
-                let videos = this.state.videos;
-                videos.push(...pager.videos);
-                this.setState({
-                    pager: pager,
-                    videos: videos
-                });
-            } catch(error) {
-                console.log(error);
-            }
-        }
-    };
 
     render() {
+        const { hasMore } = this.state
+
         const renderVideos = this.state.videos.map((video) => {
             return (
             <div className="video-list-item" 
-                onClick={() => this.props.handleVideoSelect(video)}>
+                onClick={() => this.props.handleVideoSelect(video)}
+                key={video.id}>
                 <img className="img-cover" src={video.thumbnail}/>
                 <div className="video-block-info">{video.name}</div>
             </div>);
         });
         
         return (
-        <div>
-            <div className="video-list-box">
-                {renderVideos}
-            </div>
+        <div className="infinite-scroll-container">
+                <InfiniteScroll
+                    pageStart={0} // 设置初始化请求的页数
+                    loadMore={()=>this.loadMoreData()}  // 监听的ajax请求
+                    hasMore={hasMore} // 是否继续监听滚动事件 true 监听 | false 不再监听
+                    initialLoad={false}
+                    className="video-list-box"
+                    //loader={<h4>Loading...</h4>}
+                    //endMessage={<p style={{ textAlign: 'center' }}><b>No more videos</b></p>}
+                >
+                    {renderVideos}
+                </InfiniteScroll>
         </div>
         );
     }
